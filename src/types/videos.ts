1491e25031d705e7f1d69bf0fd5ed8e6976bfe90
alpascal <alpascal@expediagroup.com>
2024-08-24 15:50:58 +0100

    feat: add basic video card with thumbnail, profile pic, title, more info button, creator name, view count and time since published

---
+++ src/types/videos.ts
@@ -0,0 +1,10 @@
+export type Video = {
+    id: string,
+    name: string,
+    duration: string,
+    iconURL: string,
+    thumbnailURL: string,
+    creatorName: string,
+    viewCount: string,
+    timeSincePublished: string
+}
