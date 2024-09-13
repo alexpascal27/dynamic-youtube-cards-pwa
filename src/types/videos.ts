610ed0e0b7dba794ec5ccc7caa2a3c2e39ec7391
Alex Pascal <32983361+alexpascal27@users.noreply.github.com>
2024-09-13 21:15:54 +0100

    feat: imitate the look of the youtube home page (#3)

---
+++ src/types/videos.ts
@@ -0,0 +1,11 @@
+export type Video = {
+    id: string,
+    duration: string,
+    name: string,
+    iconURL: string,
+    thumbnailURL: string,
+    creatorName: string,
+    isCreatorVerified: boolean,
+    viewCount: number,
+    timeSincePublished: string
+}
