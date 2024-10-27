6e7897cfb97d447e4f8fa66b5b3d4af1c5f214c5
alexpascal27 <alex.pascal27@outlook.com>
2024-10-27 17:08:14 +0000

    chore: add mdx loader packages
    chore: change mdx next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/
+});
+module.exports = withMDX({
+    pageExtensions: ['js', 'jsx', 'md', 'mdx']
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
                         <Videos tag={selectedTag}/>
                     </Box>
                 </Flex>
-                <DemoDynamicVideos />
             </ChakraProvider>
         </main>
     )
